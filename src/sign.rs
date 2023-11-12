38aedf18335bf513a0534b166d121551e87bd763
Vukasin Gostovic <vukasin@gostovic.me>
2023-11-12 13:13:31 +0300

    tests

--- src/sign.rs
+++ src/sign.rs
@@ -38,3 +38,44 @@ impl From<jsonwebtoken::errors::Error> for SignError {
         SignError::SignFailed(error.to_string())
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use jsonwebtoken::decode;
+    use jsonwebtoken::Algorithm;
+    use jsonwebtoken::DecodingKey;
+    use jsonwebtoken::Validation;
+    use serde_json::Value;
+
+    fn dummy_claims() -> Claims {
+        Claims {
+            id: Value::Number(1.into()),
+            jsonrpc: Value::String("2.0".to_string()),
+            method: Value::String("blutgang_ttl".to_string()),
+            params: Value::Array(vec![]),
+            exp: 10000000000,
+        }
+    }
+
+    #[test]
+    fn test_sign_and_verify() {
+        // Arrange
+        let token = "secret_token";
+        let claims = dummy_claims();
+
+        // Act
+        let signed_token = sign(claims.clone(), token).unwrap();
+
+        // Decode the token to verify
+        let decoded_token = decode::<Claims>(
+            &signed_token,
+            &DecodingKey::from_secret(token.as_bytes()),
+            &Validation::new(Algorithm::HS256),
+        )
+        .unwrap();
+
+        // Assert
+        assert_eq!(decoded_token.claims, claims);
+    }
+}
--- src/types.rs
+++ src/types.rs
@@ -4,7 +4,7 @@ use serde::{
 };
 use serde_json::Value;
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize, PartialEq, Clone)]
 pub struct Claims {
     pub id: Value,
     pub jsonrpc: Value,
